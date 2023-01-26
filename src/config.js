db88b5f326306dc344513edc597ae4869fac3edb
ramzan1813 <ramzanniazi85@outlook.com>
2023-01-26 19:09:16 +0500

    production envoirnament update

--- src/config.js
+++ src/config.js
@@ -10,16 +10,16 @@ const dev = {
 
 const prod = {
   baseURL: "/api/",
-  landingPageUrl: "https://app.openaitemplate.com",
+  landingPageUrl: "https://nodejstestdrive.herokuapp.com",
   stripe: {
     free: "price_1MQ9iZEKbjlHjR0pi3PIUsIc",
     entry: "price_1MQ9S1EKbjlHjR0p3sSrbAEO",
     pro: "price_1MQ9gQEKbjlHjR0pLyeXwktF",
   },
 };
-  
-const config = process.env.NODE_ENV === 'development'
-	? dev
-	: prod;
+
+const config =
+  // process.env.NODE_ENV === 'development'	? dev	:
+  prod;
   
 export default config;
