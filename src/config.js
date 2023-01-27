054f1b52697d602f250722bdabd64be45b87cb9f
ramzan1813 <ramzanniazi85@outlook.com>
2023-01-27 09:17:40 +0500

    Product Price Updated

--- src/config.js
+++ src/config.js
@@ -12,9 +12,9 @@ const prod = {
   baseURL: "/api/",
   landingPageUrl: "https://nodejstestdrive.herokuapp.com",
   stripe: {
-    free: "price_1MQ9iZEKbjlHjR0pi3PIUsIc",
-    entry: "price_1MQ9S1EKbjlHjR0p3sSrbAEO",
-    pro: "price_1MQ9gQEKbjlHjR0pLyeXwktF",
+    free: "price_1MQB54JM381soXhGbx8Vl7Aa",
+    entry: "price_1MQB7NJM381soXhGeLMRHq2N",
+    pro: "price_1MQB7zJM381soXhGwn1jDcXW",
   },
 };
 
